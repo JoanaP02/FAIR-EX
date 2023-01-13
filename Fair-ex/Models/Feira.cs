c26cd6d066d09b1bf674856ed5263d71201ac421
JoanaP02 <a97588@alunos.uminho.pt>
2023-01-13 18:59:56 +0000

    Muita cena

---
+++ Fair-ex/Models/Feira.cs
@@ -0,0 +1,13 @@
+﻿namespace Fair_ex.wwwroot.Models
+{
+    public class Feira
+    {
+        public string Id { get; set; }
+        public string tema { get; set; }
+        public DateTime dataIncricaoIninio { get; set; }
+        public DateTime dataIncricaoFim { get; set; }
+        public DateTime dataComecoInicio { get; set; }
+        public DateTime dataComecoFim { get; set; }
+        public List<Stand>? Stands { get; set; }
+    }
+}
