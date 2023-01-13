652ab6ff1ccc224fbfea971c7587e6c4b2268751
rafaelcorreia94870 <a94870@alunos.uminho.pt>
2023-01-13 19:23:45 +0000

    public class Stand

--- Fair-ex/Models/Stand.cs
+++ Fair-ex/Models/Stand.cs
@@ -2,5 +2,10 @@
 {
     public class Stand
     {
+        public Vendedor MyProperty { get; set; }
+        public float Taxa_Aceitacao { get; set; }
+        public float Taxa_rejeicao { get; set; }
+        public List<Produto> produtos { get; set; }
+        public float receita { get; set; }
     }
 }
