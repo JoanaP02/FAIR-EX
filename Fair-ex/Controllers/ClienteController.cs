3486a6d998794b3ab01bd34219d0a8e1d7c9557c
fbarros86 <xiquita.barros@gmail.com>
2023-01-14 18:41:02 +0000

    Feira e Cliente controllers

--- Fair-ex/Controllers/ClienteController.cs
+++ Fair-ex/Controllers/ClienteController.cs
@@ -1,12 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fair_ex.Models;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
 
 namespace Fair_ex.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ClienteController : Controller
     {
-        public IActionResult Index()
+        private readonly IConfiguration _config;
+        public ClienteController(IConfiguration config)
         {
-            return View();
+            _config = config;
         }
+        [HttpGet]
+        public async Task<ActionResult<List<Cliente>>> GetAllClientes()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var clientes = await connection.QueryAsync<Cliente>("select * from Cliente");
+            return Ok(clientes);
+        }
+        [HttpGet("{username}")]
+        public async Task<ActionResult<Cliente>> GetCliente(int username)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var cliente = await connection.QueryFirstAsync<Cliente>("select * from Cliente where username = @Username",
+                new { Username = username });
+            return Ok(cliente);
+        }
+        [HttpPost]
+        public async void CreateCliente(Cliente c)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into Cliente (username,password,Morada,numCartao,numTelemovel, cartaoVal,cartaoCCV, email)" +
+                " values(@Username, @Password, @Morada, @NumCartao, @Telemovel, @ValCartao, @CCV, @Email)",
+                new { Username = c.Username, Password = c.Password, Morada = c.Morada, NumCartao = c.Cartao, Telemovel = c.Telemovel,
+                      valCartao = c.validade,CCV = c.CCV, Email = c.Email});
+        }
+        [HttpPut]
+        public async void UpdateCliente(Cliente c)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update Cliente set username=@Username, password=@Password, Morada=@Morada, numCartao=@NumCartao," +
+                "       numTelemovel = @Telemovel, cartaoCCV=@CCV, email=@Email)",
+                new
+                {
+                    Username = c.Username,
+                    Password = c.Password,
+                    Morada = c.Morada,
+                    NumCartao = c.Cartao,
+                    Telemovel = c.Telemovel,
+                    CCV = c.CCV,
+                    Email = c.Email
+                });
+        }
+        [HttpDelete("{username}")]
+        public async void DeleteCliente(int username)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("delete from Cliente where username = @Username",
+                new { Username = username });
+        }
+
     }
 }
--- Fair-ex/Controllers/FeiraController.cs
+++ Fair-ex/Controllers/FeiraController.cs
@@ -1,12 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fair_ex.Models;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fair_ex.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class FeiraController : Controller
     {
-        public IActionResult Index()
+        private readonly IConfiguration _config;
+        public FeiraController(IConfiguration config)
         {
-            return View();
+            _config = config;
         }
+        [HttpGet]
+        public async Task<ActionResult<List<Feira>>> GetAllFeiras()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var feiras = await connection.QueryAsync<Feira>("select * from feira");
+            return Ok(feiras);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Feira>> GetFeira(int id)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var feira = await connection.QueryFirstAsync<Feira>("select * from feira where id = @Id",
+                new { Id = id});
+            return Ok(feira);
+        }
+        [HttpPost]
+        public async void CreateFeira(Feira f)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into feira (idfeira,tema_tema,dataDeInicioInscricao,dataDeFimInscricao,dataComeco,dataTermino)" +
+                " values(@Id, @Tema, @DII, @DFI, @DC, @DT)",
+                new
+                {
+                    Id = f.Id,
+                    Tema = f.Tema.Nome,
+                    DII = f.DataIncricaoIninio,
+                    DFI = f.DataIncricaoFim,
+                    DC = f.DataComecoInicio,
+                    DT = f.DataComecoFim
+                });
+        }
+        [HttpPut]
+        public async void UpdateFeira(Feira f)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update feira set idfeira=@Id, tema_tema=@Tema, dataDeInicioInscricao=@DII," +
+                "dataDeFimInscricao= @DFI,dataComeco= @DC, dataTermino=@DT)",
+                new
+                {
+                    Id = f.Id,
+                    Tema = f.Tema.Nome,
+                    DII = f.DataIncricaoIninio,
+                    DFI = f.DataIncricaoFim,
+                    DC = f.DataComecoInicio,
+                    DT = f.DataComecoFim
+                });
+        }
+        [HttpDelete("{id}")]
+        public async void DeleteFeira(int id)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("delete from feira where idfeira = @Id",
+                new { Id = id });
+        }
+
     }
 }
