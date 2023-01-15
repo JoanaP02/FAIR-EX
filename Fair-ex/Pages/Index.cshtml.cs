3390f6ba05e206dfcf3a1a6e5ea06c7c5eaf9d60
Robert Szabo <roobertsz@gmail.com>
2023-01-15 22:38:26 +0000

    pouca cena

--- Fair-ex/Pages/Index.cshtml.cs
+++ Fair-ex/Pages/Index.cshtml.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Fair_ex.Models;
+using Fair_ex.Services;
 
 namespace Fair_ex.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public List<Stand> stands { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public async void OnGet()
         {
-
+            StandService service = new StandService();
+            stands = await service.GetAllStands();
         }
     }
 }
--- Fair-ex/Services/ProdutoService.cs
+++ Fair-ex/Services/ProdutoService.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
 using Dapper;
 using Microsoft.AspNetCore.Hosting.Server;
-using static Slapper.AutoMapper;
 
 namespace Fair_ex.Services
 {
